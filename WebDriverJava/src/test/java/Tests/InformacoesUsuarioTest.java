eb3f8f59fc0c4c34b3a359bd73db9a68963fd1e7
GzSphynx <91573168+GzSphynx@users.noreply.github.com>
2021-10-19 22:14:49 -0300

    Automação Selenium

--- WebDriverJava/src/test/java/Tests/InformacoesUsuarioTest.java
+++ WebDriverJava/src/test/java/Tests/InformacoesUsuarioTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -30,40 +31,58 @@ public class InformacoesUsuarioTest {
         navegador.manage().window().maximize();
 
         // Inserindo a Url que deseja acessar
-        navegador.get("https://www.facebook.com");
+        navegador.get("http://www.juliodelima.com.br/taskit/");
     }
 
     @Test
     public void testAdcionarUmaInformacaoAdicionalDoUsuario(){
-        //Clicar no campo e-mail
-        WebElement campoEmail = navegador.findElement(By.id("email"));
-        campoEmail.click();
+        //Clicar no link de nome: "Sign in"
+        navegador.findElement(By.linkText("Sign in")).click();
 
-        //Digitar no campo o email felipe.rossigz@hotmail.com
-        campoEmail.sendKeys("felipe.rossigz@hotmail.com");
+        //Identificar o formulário de login
+        WebElement formLogin = navegador.findElement(By.id("signinbox"));
 
-        //Clicar no campo senha
-        WebElement campoSenha = navegador.findElement(By.id("pass"));
-        campoSenha.click();
+        //Inserir o user: "julio0001" no campo de login de name: "login"
+        formLogin.findElement(By.name("login")).sendKeys("julio0001");
 
-        //Digitar a senha
-        campoSenha.sendKeys("BsCOgLCQdf");
+        //Inserir a senha: "123456" no campo de senha de name: "password"
+        formLogin.findElement(By.name("password")).sendKeys("123456");
 
-        //Clicar no botão sign in
-        navegador.findElement(By.name("login")).click();
+        //Clicar no botão: "Sign in" do texto: "SIGN IN"
+        formLogin.findElement(By.linkText("SIGN IN")).click();
 
-        //Validar o nome "Felipe" após o login
-        WebElement AreaNome = navegador.findElement(By.linkText("Felipe Rossi"));
-        String nomedoUsuario = AreaNome.getText();
+        //Clicar no link que tenha a class: "me"
+        navegador.findElement(By.className("me")).click();
 
-        // Verificar se o teste foi positivo ou negativo
-        assertEquals("Felipe Rossi",nomedoUsuario);
+        //Clicar no link com o texto: "MORE DATA ABOUT YOU"
+        navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
+
+        //Clicar no botão atraves do seu Xpath:"//button[@data-target=\"addmoredata""));
+        navegador.findElement(By.xpath("//button[@data-target=\"addmoredata\"]")).click();
+
+        //Identificar o formulário para adicionar mais dados
+        WebElement formAddMoreData = navegador.findElement(By.id("addmoredata"));
+
+        //Pegando o name do campo de seleção e setando para a opção Phone
+        WebElement campoDeSelecao = formAddMoreData.findElement(By.name("type"));
+        new Select(campoDeSelecao).selectByValue("phone");
+
+        //Preenchedo o  campo Contato de name: "contact" com o número:"+551191234-5678"
+        formAddMoreData.findElement(By.name("contact")).sendKeys("+551191234-5678");
+
+        //Clicando no link save de texto: "SAVE"
+        formAddMoreData.findElement(By.linkText("SAVE")).click();
+
+        //Validar que a mensagem exibida seja: "Your contact has been added!"
+        WebElement mensagemPopUp = navegador.findElement(By.id("toast-container"));
+        String textoExibido = mensagemPopUp.getText();
+        assertEquals("Your contact has been added!", textoExibido);
     }
 
     @After
     public void tearDown(){
         //Fechar o navegador
-        navegador.close();
+        //navegador.close();
     }
     /*@Test
     public void LoginVivo(){
